8304683cee1cd2debd6cc02a145485b216f63a5b
ericfurquim05-source <eric.furquim05@gmail.com>
2026-09-16 20:15:38 -0300

    Atualiza cache para cálculo final da aposentadoria

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = 'simulador-consorcio-v2.5.1-aposentadoria-financeira-v11';
+const VERSION = 'simulador-consorcio-v2.5.1-aposentadoria-financeira-v12';
 const STATIC_CACHE = `${VERSION}-static`;
 
 const ASSETS = [
