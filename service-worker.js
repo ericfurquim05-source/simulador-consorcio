b4bbeb74b65cc2f8d78d123913f0a1215338609b
ericfurquim05-source <eric.furquim05@gmail.com>
2026-09-16 20:23:58 -0300

    Atualiza cache da aposentadoria para v13

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = 'simulador-consorcio-v2.5.1-aposentadoria-financeira-v12';
+const VERSION = 'simulador-consorcio-v2.5.1-aposentadoria-financeira-v13';
 const STATIC_CACHE = `${VERSION}-static`;
 
 const ASSETS = [
